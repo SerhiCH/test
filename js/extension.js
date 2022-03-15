e203aaa3ea13aad7724905a12f4afcdab35758d2
SerhiiCH <serhiicherniakwork@gmail.com>
2022-03-16 00:10:44 +0200

    burger menu

--- js/extension.js
+++ js/extension.js
@@ -1,13 +1,21 @@
 // дополнение для еффекта кручения glassmorphic-card
-VanillaTilt.init(document.querySelector(".glassmorphic-card"), {
-    max: 15,
-    speed: 200,
-    glare: true,
-    "max-glare": 1,
-});
+// VanillaTilt.init(document.querySelector(".glassmorphic-card"), {
+//     max: 15,
+//     speed: 200,
+//     glare: true,
+//     "max-glare": 1,
+// });
 
 
-// по клику на кнопку открывать url
+// по клику на кнопку 'button' открывать то что указано в url в HTML
 $('button').on('click', function(){     
     window.location.href = $(this).attr('url');
+});
+
+//Burger menu
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
 });
